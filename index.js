b6ee3587782e08085ef998a2a0b64c251d0b1143
Shamim <shamim@outlook.jp>
2023-05-23 01:46:12 +0600

    ready to push

---
+++ index.js
@@ -0,0 +1,31 @@
+
+const express = require('express');
+const app = express();
+var cors = require('cors');
+const port = 5000;
+
+app.use(cors());
+
+const chefs = require("./data/data.json")
+const slides = require("./data/slides.json")
+
+app.get('/', (req, res) => {
+    res.send(`This server is running on port: ${port}`)
+});
+app.get("/chefs", (req, res) => {
+    res.send(chefs)
+})
+app.get("/slides", (req, res) => {
+    res.send(slides)
+})
+
+app.get("/chefs/:id", (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+    const selectedId = chefs.find(n => n.id === id);
+    res.send(selectedId);
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
